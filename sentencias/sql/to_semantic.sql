13fd727317ab417f3a40deb6e34bc809a093783c
SCS <s.cadavid1587@hotmail.com>
2019-12-11 19:34:04 -0600

    se documentó y corrigió sintaxis de semantic

--- sentencias/sql/to_semantic.sql
+++ sentencias/sql/to_semantic.sql
@@ -1,65 +1,111 @@
 -- Crear tabla de entidades
 -- borrar la tabla en caso de que ya exista y crear una nueva
-  drop table if exists semantic.entities;
-  create table if not exists semantic.entities as (
-    select
-      "offender",
-      "country_citizenship",
-      make_date(birth_year, birth_month,01) as birth_date,
-      "genre",
-      "race"
-      from cleaned.sentencias2017 );
--- índices para la tabla de entidades
-create index semantic_entities_offender_ix on semantic.entities(offender);
-create index semantic_entities_citizenship_ix on semantic.entities(country_citizenship);
-create index semantic_entities_birth_ix on semantic.entities(birth_date);
-create index semantic_entities_genre_ix on semantic.entities(genre);
-create index semantic_entities_race_ix on semantic.entities(race);
+
+DROP TABLE IF EXISTS semantic.entities;
+
+
+CREATE TABLE IF NOT EXISTS semantic.entities AS
+  (SELECT "offender",
+          "country_citizenship",
+          make_date(birth_year, birth_month, 01) AS birth_date,
+          "genre",
+          "race"
+   FROM cleaned.sentencias2017);
+
+-- índices asociados a la tabla de eventos
+
+CREATE INDEX semantic_entities_offender_ix ON semantic.entities(offender);
+CREATE INDEX semantic_entities_citizenship_ix ON semantic.entities(country_citizenship);
+CREATE INDEX semantic_entities_birth_ix ON semantic.entities(birth_date);
+CREATE INDEX semantic_entities_genre_ix ON semantic.entities(genre);
+CREATE INDEX semantic_entities_race_ix ON semantic.entities(race);
 
 ---- Tablas de eventos:
--- Evento 1: Se le genera la sentencia al acusado
-drop table if exists semantic.event_sentences;
-create table if not exists semantic.event_sentences as (
-  select "offender", "sentencing_date","sentence_type", "district_sentence",
-  case when sentence_type = 'no prison or probation ' then concat('sentenced to',' ',0,' ','months in prison')
-  when sentence_type = 'prison only ' and "imprisonment_length" is not null and "imprisonment_length"<>'life' then concat('sentenced to',' ',"imprisonment_length"::float,' ', 'months in prison')
-  when sentence_type = 'prison and confinement conditions 'and "sensplt" is not null then concat('sentenced to',' ',"sensplt"::float,' ','months in prison')
-  when sentence_type = 'probation and confinement conditions ' and "total_sentence_length" is not null and "imprisonment_length"<>'life' and "imprisonment_length" is not null then concat('sentenced to',' ',"total_sentence_length"::float-"imprisonment_length"::float,' ','months in prison')
-  when sentence_type = 'probation only' then concat('sentenced to',' ',0,' ','months in prison')
-  else 'not enough information'
-  end as attributes
-  from cleaned.sentencias2017);
--- índices asociados
-create index semantic_sentences_offender_ix on semantic.event_sentences(offender);
-create index semantic_sentences_date_ix on semantic.event_sentences(sentencing_date);
-create index semantic_sentences_type_ix on semantic.event_sentences(sentence_type);
-create index semantic_sentences_district_ix on semantic.event_sentences(district_sentence);
+-- Evento 1: Se le genera la sentencia AL ACUSADO
+-- para la creación de la sentencia se muestra la fecha, lugar, tipo, y como atributo
+-- adicional los meses de condena.
 
-  -- Evento 2: Se le genera la reincidencia
-  drop table if exists semantic.event_reincidence;
-  create table if not exists semantic.event_reincidence as (
-    select "offender", "sentencing_date","sentence_type", "district_sentence", "criminal_history",
-    case when criminal_history = 'yes' then 'with previous criminal history'
-    when criminal_history = 'no' then 'without previous criminal history'
-    else null
-    end as attributes
-    from cleaned.sentencias2017);
-  -- índices asociados
-  create index semantic_reincidence_offender_ix on semantic.event_reincidence(offender);
-  create index semantic_reincidence_date_ix on semantic.event_reincidence(sentencing_date);
-  create index semantic_reincidence_type_ix on semantic.event_reincidence(sentence_type);
-  create index semantic_reincidence_district_ix on semantic.event_reincidence(district_sentence);
-  create index semantic_reincidence_criminalhist_ix on semantic.event_reincidence(criminal_history);
 
-    -- Evento 3: Se le generó una multa al sentenciado
-    drop table if exists semantic.event_fine;
-    create table if not exists semantic.event_fine as (
-      select "offender", "sentencing_date", "district_sentence", "total_amount",
-      concat('the fine amount was', ' ',"total_amount", ' ','USD', ' ', 'for a citizen from', ' ',"country_citizenship") as attributes
-      from cleaned.sentencias2017
-      where total_amount is not null);
-    -- índices asociados
-    create index semantic_fine_offender_ix on semantic.event_fine(offender);
-    create index semantic_fine_date_ix on semantic.event_fine(sentencing_date);
-    create index semantic_fine_amount_ix on semantic.event_fine(total_amount);
-    create index semantic_fine_district_ix on semantic.event_fine(district_sentence);
+DROP TABLE IF EXISTS semantic.event_sentences;
+
+
+CREATE TABLE IF NOT EXISTS semantic.event_sentences AS
+  (SELECT "offender",
+          "sentencing_date",
+          "sentence_type",
+          "district_sentence",
+          CASE
+              WHEN sentence_type = 'no prison or probation ' THEN concat('sentenced to', ' ', 0, ' ', 'months in prison')
+              WHEN sentence_type = 'prison only '
+                   AND "imprisonment_length" IS NOT NULL
+                   AND "imprisonment_length"<>'life' THEN concat('sentenced to', ' ', "imprisonment_length"::float, ' ', 'months in prison')
+              WHEN sentence_type = 'prison and confinement conditions '
+                   AND "sensplt" IS NOT NULL THEN concat('sentenced to', ' ', "sensplt"::float, ' ', 'months in prison')
+              WHEN sentence_type = 'probation and confinement conditions '
+                   AND "total_sentence_length" IS NOT NULL
+                   AND "imprisonment_length"<>'life'
+                   AND "imprisonment_length" IS NOT NULL THEN concat('sentenced to', ' ', "total_sentence_length"::float-"imprisonment_length"::float, ' ', 'months in prison')
+              WHEN sentence_type = 'probation only' THEN concat('sentenced to', ' ', 0, ' ', 'months in prison')
+              ELSE 'not enough information'
+          END AS attributes
+   FROM cleaned.sentencias2017);
+
+-- índices asociados a la tabla de eventos
+
+CREATE INDEX semantic_sentences_offender_ix ON semantic.event_sentences(offender);
+CREATE INDEX semantic_sentences_date_ix ON semantic.event_sentences(sentencing_date);
+CREATE INDEX semantic_sentences_type_ix ON semantic.event_sentences(sentence_type);
+CREATE INDEX semantic_sentences_district_ix ON semantic.event_sentences(district_sentence);
+
+-- Evento 2: El acusado presenta reincidencia
+-- se genera como evento en el que se meustra el acusado, la fecha de sentencia,
+-- el tipo de ofensa, el lugar (distrito), y si tenía historia criminal.
+-- Esto con la búsqueda de determinar si reincide o es primera vez (atributo).
+
+DROP TABLE IF EXISTS semantic.event_reincidence;
+
+
+CREATE TABLE IF NOT EXISTS semantic.event_reincidence AS
+  (SELECT "offender",
+          "sentencing_date",
+          "sentence_type",
+          "district_sentence",
+          "criminal_history",
+          CASE
+              WHEN criminal_history = 'yes' THEN 'with previous criminal history'
+              WHEN criminal_history = 'no' THEN 'without previous criminal history'
+              ELSE NULL
+          END AS attributes
+   FROM cleaned.sentencias2017);
+
+-- índices asociados a la tabla de eventos
+
+CREATE INDEX semantic_reincidence_offender_ix ON semantic.event_reincidence(offender);
+CREATE INDEX semantic_reincidence_date_ix ON semantic.event_reincidence(sentencing_date);
+CREATE INDEX semantic_reincidence_type_ix ON semantic.event_reincidence(sentence_type);
+CREATE INDEX semantic_reincidence_district_ix ON semantic.event_reincidence(district_sentence);
+CREATE INDEX semantic_reincidence_criminalhist_ix ON semantic.event_reincidence(criminal_history);
+
+-- Evento 3: Se le generó una multa al sentenciado
+-- Se genera un evento en el cual se muestra el acusado, la fecha de la sentencia,
+-- el lugar (distrito), y la multa generada. Como descripción del evento se
+-- muestra la ciudadanía del acusado.
+
+DROP TABLE IF EXISTS semantic.event_fine;
+
+
+CREATE TABLE IF NOT EXISTS semantic.event_fine AS
+  (SELECT "offender",
+          "sentencing_date",
+          "district_sentence",
+          "total_amount",
+          concat('the fine amount was', ' ', "total_amount", ' ', 'USD', ' ', 'for a citizen from', ' ', "country_citizenship") AS attributes
+   FROM cleaned.sentencias2017
+   WHERE total_amount IS NOT NULL);
+
+-- índices asociados a la tabla de eventos
+
+CREATE INDEX semantic_fine_offender_ix ON semantic.event_fine(offender);
+CREATE INDEX semantic_fine_date_ix ON semantic.event_fine(sentencing_date);
+CREATE INDEX semantic_fine_amount_ix ON semantic.event_fine(total_amount);
+CREATE INDEX semantic_fine_district_ix ON semantic.event_fine(district_sentence);
